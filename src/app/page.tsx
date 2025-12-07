3052a7556b886a5fe5464cbe1164a06cbd8449ad
Ashmit-k <ashmitk070@gmail.com>
2025-12-07 20:51:39 +0530

    Animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client"
 import FooterDivider from "@/components/layout/Footer";
 import AboutMeSection from "@/components/sections/AboutMeSection";
 import ContactSection from "@/components/sections/Contact";
 import Experience from "@/components/sections/Experience";
 import HeroSection from "@/components/sections/HeroSection";
 import LatestWorks from "@/components/sections/LatestWorks";
+import { motion, useScroll } from "framer-motion";
 export default function Home() {
-  return <><HeroSection />
-  <Experience />
-  <LatestWorks />
-  <AboutMeSection />
-  <ContactSection />
-  <FooterDivider/>
-  </>;
+
+  const  scrollYProgress  = useScroll().scrollYProgress;
+  return <>
+    <motion.div
+      style={
+        {
+          scaleX: scrollYProgress
+        }
+      }
+      className="bg-[#FF4500] origin-left w-full h-[6px] z-100 top-0 left-0 fixed"
+    ></motion.div>
+    <HeroSection />
+    <Experience />
+    <LatestWorks />
+    <AboutMeSection />
+    <ContactSection />
+    <FooterDivider />
+
+  </>
 }
